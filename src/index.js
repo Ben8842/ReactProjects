b3a8fa0945908266f331a8899fb687df7ae7bfd4
Ben8842 <z87e99b44r55a@gmail.com>
2020-10-25 22:13:23 -0400

    fixed some of the  clock project

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ ReactDOM.render(
 function tick() {
   const element = (
     <div>
-      <h1>Hello, world! </h1>
+      
       <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
   );
