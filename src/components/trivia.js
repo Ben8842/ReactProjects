8e2354bb5b816bf5d751d9574757bdfb2118a2c9
Ben8842 <z87e99b44r55a@gmail.com>
2020-12-22 19:55:54 -0500

    added logic for completing and scoring the trivia added logic for correct and incorrect answers added css and added screens for success and wrong answers

--- src/components/trivia.js
+++ src/components/trivia.js
@@ -9,12 +9,15 @@ class trivia extends Component {
       triviaData: [],
       show: false,
       count: 1,
+      isCorrectChoice: false,
+      isWrongChoice: false,
+      correctCount: 0,
     };
   }
 
   componentDidMount() {
     fetch(
-      "https://opentdb.com/api.php?amount=10&category=30&type=multiple&encode=base64"
+      "https://opentdb.com/api.php?amount=15&category=30&type=multiple&encode=base64"
     )
       .then((res) => res.json())
       .then((json) => {
@@ -31,6 +34,8 @@ class trivia extends Component {
 
   nextTrivia() {
     this.incrementCount();
+    this.setState({ isCorrectChoice: false });
+    this.setState({ isWrongChoice: false });
   }
 
   incrementCount() {
@@ -39,17 +44,30 @@ class trivia extends Component {
     });
   }
 
-  isCorrect(ans) {
-    var { triviaData, count } = this.state;
-    if (atob(triviaData.results[count].correct_answer) === ans) {
-      console.log("CORRECT");
-    } else {
-      console.log("SO MUCH WRONG");
-      console.log(atob(triviaData.results[count].correct_answer));
-      console.log(ans);
-    }
+  incrementCorrectCount() {
+    this.setState((state) => {
+      return { correctCount: state.count + 1 };
+    });
+  }
 
-    console.log("hello there");
+  isCorrect(ans) {
+    var {
+      triviaData,
+      count,
+      isCorrectChoice,
+      isWrongChoice,
+      correctCount,
+    } = this.state;
+    if (atob(triviaData.results[count].correct_answer) === ans) {
+      this.incrementCorrectCount();
+      this.setState({
+        isCorrectChoice: true,
+      });
+    } else {
+      this.setState({
+        isWrongChoice: true,
+      });
+    }
   }
 
   displayTrivia() {
@@ -70,7 +88,13 @@ class trivia extends Component {
   }
 
   displayTriviaA() {
-    var { isLoaded, triviaData, count } = this.state;
+    var {
+      isLoaded,
+      triviaData,
+      count,
+      isCorrectChoice,
+      isWrongChoice,
+    } = this.state;
     if (isLoaded) {
       var firstAnswer = atob(triviaData.results[count].correct_answer);
       var secondAnswer = atob(triviaData.results[count].incorrect_answers[0]);
@@ -117,11 +141,42 @@ class trivia extends Component {
   }
 
   render() {
-    var { triviaData, isLoaded, show, count } = this.state;
+    var {
+      triviaData,
+      isLoaded,
+      show,
+      count,
+      isCorrectChoice,
+      isWrongChoice,
+      correctCount,
+    } = this.state;
     console.log(triviaData.results);
 
     const question = this.displayTrivia();
     const answer = this.displayTriviaA();
+    const celebration = (
+      <div id="correct">
+        <p id="correct">You got this question number {count} correct! </p>
+        <p>{question}</p>
+        <p id="correct">! CELEBRATION !</p>
+        <p>Continue to the next Question</p>
+        <button class="button button3" onClick={() => this.nextTrivia()}>
+          Next Question
+        </button>
+      </div>
+    );
+
+    const recuperate = (
+      <div id="wrong">
+        <p id="wrong">You got this question number {count} Wrong! </p>
+        <p>Don't give up yet. </p>
+        <p id="wrong">Continue to the next Question</p>
+        <button class="button button2" onClick={() => this.nextTrivia()}>
+          Next Question
+        </button>
+      </div>
+    );
+
     const questionBox = (
       <div id="trivia">
         <p id="trivia">QUESTION {count} </p>
@@ -130,6 +185,24 @@ class trivia extends Component {
         <p>{answer}</p>
       </div>
     );
+
+    const runningScore = (
+      <p id="tab">
+        {correctCount} answers correct out of {count} questions
+      </p>
+    );
+
+    const triviaComplete = (
+      <div id="trivia">
+        <p id="trivia">Trivia Complete! </p>
+        <p>
+          Your score is {correctCount} of {count}
+        </p>
+        <p id="trivia">Thank you so much for playing</p>
+        <p>Try again soon!</p>
+      </div>
+    );
+
     if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
@@ -139,16 +212,15 @@ class trivia extends Component {
             <button class="button button1" onClick={() => this.handleClick()}>
               Start Trivia
             </button>
-            <button class="button button1" onClick={() => this.nextTrivia()}>
-              Next Question
-            </button>
+
+            {isLoaded && show ? runningScore : null}
           </div>
-          <p>Question</p>
-          {isLoaded && show ? questionBox : null}
-
-          <p>Enter your Answer</p>
-
-          <button>SUBMIT</button>
+          {count === 10 ? triviaComplete : null}
+          {isLoaded && show && !isCorrectChoice && !isWrongChoice
+            ? questionBox
+            : null}
+          {isCorrectChoice ? celebration : null}
+          {isWrongChoice ? recuperate : null}
         </body>
       );
     }
